01d0fa460bfeb0a6104b29b7ffbecd1a4bbbe176
nevertry <nevertry@famnote.com>
2020-01-13 10:19:27 +0900

    First commit

--- backend/config/production.js
+++ backend/config/production.js
@@ -5,28 +5,28 @@ module.exports = {
    * PostgreSQL configuration for Sequelize.
    * More info: https://sequelize.org/v5/manual/getting-started.html#setting-up-a-connection
    */
-  database: {
-    username: 'postgres',
-    dialect: 'postgres',
-    password: '',
-    database: 'postgres',
-    host: 'postgres',
-    logging: console.log,
-  },
+  //database: {
+  //  username: 'postgres',
+  //  dialect: 'postgres',
+  //  password: '',
+  //  database: 'postgres',
+  //  host: 'postgres',
+  //  logging: console.log,
+  //},
 
   /**
    * MySQL configuration for Sequelize.
    * More info: https://sequelize.org/v5/manual/getting-started.html#setting-up-a-connection
    */
-  // database: {
-  //   username: 'root',
-  //   dialect: 'mysql',
-  //   password: '',
-  //   database: 'production',
-  //   host:
-  //     '<insert public ip here>',
-  //   logging: console.log,
-  // },
+   database: {
+     username: 'root',
+     dialect: 'mysql',
+     password: '',
+     database: 'production',
+     host:
+       '<insert public ip here>',
+     logging: console.log,
+   },
 
   /**
    * Secret used to Sign the JWT (Authentication) tokens.
